0b16a08b5da3ba3f44eee9037903afdde419a1dc
stavroskoumpounis <skoumpounis@gmail.com>
2022-10-04 21:21:08 +0100

    added ipfs and pinata pin

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,17 +5,17 @@ const main = async () => {
   
     console.log("Contract deployed to:", domainContract.address);
   
-    // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-    let txn = await domainContract.register("magician",  {value: hre.ethers.utils.parseEther('0.05')});
-    await txn.wait();
-    console.log("Minted domain magician.keftes");
+    // // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
+    // let txn = await domainContract.register("magician",  {value: hre.ethers.utils.parseEther('0.05')});
+    // await txn.wait();
+    // console.log("Minted domain magician.keftes");
   
-    txn = await domainContract.setRecord("magician", "Am I a magician or a keftes??");
-    await txn.wait();
-    console.log("Set record for magician.keftes");
+    // txn = await domainContract.setRecord("magician", "Am I a magician or a keftes??");
+    // await txn.wait();
+    // console.log("Set record for magician.keftes");
   
-    const address = await domainContract.getAddress("magician");
-    console.log("Owner of domain magician:", address);
+    // const address = await domainContract.getAddress("magician");
+    // console.log("Owner of domain magician:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
